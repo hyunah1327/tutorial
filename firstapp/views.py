7815556654ece5d8a500936388c72ac7ba19ed7d
hyunah <gusdk13272@gmail.com>
2022-05-11 15:04:18 +0900

    커밋

---
+++ firstapp/views.py
@@ -0,0 +1,18 @@
+# from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index1(request):
+    return HttpResponse('<u>Hello</u>')
+
+def index2(request):
+    return HttpResponse('<u>Hi</u>')
+
+def main(request):
+    return HttpResponse('<u>Main</u>')
+
+def home(request):
+    return HttpResponse('Home')
